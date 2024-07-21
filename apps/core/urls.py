274b016920437e989598916a912e2e152108c8dd
Alejandro <josealejandrogomezparedes@gmail.com>
2024-07-21 11:39:41 -0500

    add: AI asistant module

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,28 +1,21 @@
 from django.urls import path
-from apps.core.views import collage, home, about, profile, logs,team,chat
+from apps.core.views import (
+    collage, home, about, profile, logs, team, chat, asist)
 from django.conf.urls.static import static
 from django.conf import settings
-# from apps.core.models import User
-# from django.http import HttpResponse
-
-
-# def print_user_permissions(request):
-#     user = User.objects.get(username=request.user.username)
-#     permissions = user.get_all_permissions()
-#     print("Permisos del usuario {}: {}".format(user.username, permissions))
-#
-#     return HttpResponse("Permisos impresos en la consola.")
-
 
 app_name = 'core'
 urlpatterns = [
     path('', home.HomeTemplateView.as_view(), name="home"),
+    path('asist/', asist.AsistTemplateView.as_view(), name="asist"),
     path('collage/', collage.CollageTemplateView.as_view(), name="collage"),
+    path('profile/', profile.ProfileTemplateView.as_view(), name="profile"),
     path('about/', about.AboutTemplateView.as_view(), name="about"),
-    path('data_response/', about.TeacherDataResponse.as_view(), name="response"),
+    path('data_response/', about.TeacherDataResponse.as_view(),
+         name="response"),
     path('team/', team.TeamTemplateView.as_view(), name="team"),
     path('chat/', chat.ChatTemplateView.as_view(), name="chat"),
     path('withoutpermissions/', logs.NoPermissions.as_view(),
          name="withoutpermissions"),
-    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
