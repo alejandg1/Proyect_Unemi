b5e7eb65b21671a7e39b3286ccc774f2d3d5295f
Scherpschutter <konrdlisthrop@gmail.com>
2024-07-24 19:45:21 -0500

    fix: team view and login

--- apps/core/views/logs.py
+++ apps/core/views/logs.py
@@ -1,11 +1,14 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth import logout
 from django import http
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from apps.core.forms import LoginForm
 
-
-class LoginTemplateView(TemplateView):
+class LoginTemplateView(LoginView):    
+    form_class = LoginForm
     template_name = 'index/login.html'
-
+    next_page = reverse_lazy('core:home')
 
 class NoPermissions(TemplateView):
     template_name = "components/NoPermissions.html"
