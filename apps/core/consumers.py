bf1255b4e5a7682a39907b1a573f22291b64b802
Alejandro <josealejandrogomezparedes@gmail.com>
2024-07-25 17:59:40 -0500

    add: create collage

--- apps/core/consumers.py
+++ apps/core/consumers.py
@@ -1,4 +1,5 @@
 import json
+import os
 from channels.generic.websocket import AsyncWebsocketConsumer
 import base64
 from django.utils import timezone
@@ -9,26 +10,32 @@ import uuid
 import aiohttp
 from PIL import Image
 from asgiref.sync import sync_to_async
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def save_image_sync(user, image_data, filename):
     image_model = GeneratedImage()
     image_model.user = user
-    image_model.Img.save(name=filename, content=ContentFile(image_data, name=filename))
+    image_model.Img.save(
+        name=filename, content=ContentFile(image_data, name=filename))
     image_model.save()
     return image_model.Img.url
+
 
 class DallEChat(AsyncWebsocketConsumer):
     async def connect(self):
         print('Conectandose al websocket...')
-        
+
         self.user_id = self.scope['user'].id
         self.room_name = f'session_{self.user_id}'
         print(f'Asignando conexión en la sala: {self.room_name}')
-        
+
         await self.channel_layer.group_add(self.room_name, self.channel_name)
         await self.accept()
-        
-        print(f"Conexión aceptada para el usuario con ID {self.user_id} en la sala: {self.room_name}")
+
+        print(f"Conexión aceptada para el usuario con ID {
+              self.user_id} en la sala: {self.room_name}")
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
@@ -49,7 +56,7 @@ class DallEChat(AsyncWebsocketConsumer):
                 'img64': img64.split(',')[1],
                 'datetime': timezone.localtime(timezone.now()).strftime('%Y-%m-%d %H:%M:%S')
             })
-            
+
         except json.JSONDecodeError as e:
             print(f"Error al decodificar JSON: {e}")
         except Exception as e:
@@ -61,9 +68,9 @@ class DallEChat(AsyncWebsocketConsumer):
         sender_id = event['sender_id']
         img64 = event['img64']
         current_user_id = self.scope['user'].id
-        
+
         response, condition = await self.dalle_response(message, img64, int(sender_id))
-        
+
         if sender_id == current_user_id:
             await self.send(text_data=json.dumps({
                 'type': 'chat_message',
@@ -73,21 +80,21 @@ class DallEChat(AsyncWebsocketConsumer):
                 'datetime': datetime
             }))
             print("Mensaje enviado")
-        
+
     async def dalle_response(self, message, img64, user_id):
         try:
-            
+
             user = await sync_to_async(User.objects.get)(id=user_id)
-            
+
             decoded = base64.b64decode(img64)
             image_io = BytesIO(decoded)
-            
+
             with Image.open(image_io) as img:
                 img = img.convert('RGBA')
                 img_io = BytesIO()
                 img.save(img_io, format='PNG')
                 img_io.seek(0)
-                image_data = img_io.getvalue()    
+                image_data = img_io.getvalue()
 
             async with aiohttp.ClientSession() as session:
                 async with session.post(
@@ -96,7 +103,9 @@ class DallEChat(AsyncWebsocketConsumer):
                         'image': image_data,
                         'text': str(message),
                     },
-                    headers={'api-key': '813cf899-d542-4f56-b4a0-2a17eeb88c57'}
+                    # headers={'api-key':
+                    # '813cf899-d542-4f56-b4a0-2a17eeb88c57'}
+                    headers={'api-key': os.getenv('DEEP_KEY')}
                 ) as response:
                     if response.status != 200:
                         print(f"Error al consultar con la API de DeepAI: {await response.text()}")
@@ -111,13 +120,13 @@ class DallEChat(AsyncWebsocketConsumer):
                         return None, False
 
                     new_image_data = await deep_image.read()
-            
+
             filename = f'image_{uuid.uuid4()}.png'
-            
+
             img_url = await sync_to_async(save_image_sync)(user, new_image_data, filename)
-            
+
             return img_url, True
-            
+
         except Exception as e:
             print(f"Error al generar/guardar la imagen: {e}")
             return None, False
--- apps/core/views/collage.py
+++ apps/core/views/collage.py
@@ -1,7 +1,10 @@
 from django.views.generic import TemplateView
+from pathlib import Path
+from django.shortcuts import render as Render
+import os
+from django.conf import settings
+from PIL import Image
 from django.shortcuts import redirect
-from django.http import JsonResponse
-from django.core.paginator import Paginator
 from apps.core.models import User, GeneratedImage
 
 
@@ -9,7 +12,7 @@ class CollageTemplateView(TemplateView):
     template_name = 'index/collage.html'
 
     def get(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
+        if not request.user.is_superuser:
             return redirect('core:home')
         else:
             return super().get(request, *args, **kwargs)
@@ -34,10 +37,38 @@ def separar_imagenes(images):
         print(e)
 
 
+def collage_pill(group_img):
+    images = []
+    UnemiIMG = Image.open(os.path.join(settings.MEDIA_ROOT, 'unemi.png'))
+    for img in group_img:
+        url = f'{settings.BASE_DIR}{img}'
+        images.append(Image.open(url))
+    countFiles = os.listdir(os.path.join(settings.MEDIA_ROOT, 'collages'))
+    collageURL = os.path.join(
+        settings.MEDIA_ROOT, 'collages', f'collage{len(countFiles)+1}.png')
+    collage_W = max([img.width for img in images])*4
+    collage_H = sum([img.height for img in images])*3
+    collage = Image.new('RGB', (collage_W, collage_H))
+    centralPosition = (collage_W//4, collage_H//3)
+    collage.paste(UnemiIMG, centralPosition)
+    positions = [(x*collage_W//4, y*collage_H//3)
+                 for x in range(4) for y in range(3)]
+    for img, pos in zip(images, positions):
+        collage.paste(img, pos)
+    collage.save(collageURL)
+    return collageURL
+
+
 def MakeCollage(request):
     users = User.objects.filter(is_temp=True)
     images = GeneratedImage.objects.filter(user__in=users)
     grupos = []
     grupos = separar_imagenes(images)
-    print(grupos)
-    return JsonResponse({'images': grupos})
+    superusers = User.objects.filter(is_superuser=True)
+    collages = []
+    for group in grupos:
+        NewCollage = collage_pill(group)
+        collages.append(NewCollage)
+        for user in superusers:
+            GeneratedImage.objects.create(user=user, Img=NewCollage)
+    return Render(request, 'index/collage.html', {'collages': collages})
