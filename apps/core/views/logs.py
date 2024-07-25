46064177b1e95102c4f2a1d93c1e58eecb3c0123
Alejandro <josealejandrogomezparedes@gmail.com>
2024-07-24 22:31:03 -0500

    add: funct to delete users

--- apps/core/views/logs.py
+++ apps/core/views/logs.py
@@ -1,22 +1,21 @@
 from django.views.generic.base import TemplateView
-from django.contrib.auth import logout
-from django import http
+from apps.core.models import User
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from apps.core.forms import LoginForm
 
-class LoginTemplateView(LoginView):    
+
+class LoginTemplateView(LoginView):
     form_class = LoginForm
     template_name = 'index/login.html'
     next_page = reverse_lazy('core:home')
+
 
 class NoPermissions(TemplateView):
     template_name = "components/NoPermissions.html"
 
 
-def Logout(request):
-    if request.user.is_temp:
-        request.user.delete()
-        request.session.flush()
-    logout(request)
-    return http.HttpResponse("Logged out successfully!")
+def DelTemps(request):
+    users = User.objects.filter(is_temp=True)
+    for user in users:
+        user.delete()
