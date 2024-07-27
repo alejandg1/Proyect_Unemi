eb5a7d8fe1a4076d00cb8d0a0665de10959aaa54
Alejandro <josealejandrogomezparedes@gmail.com>
2024-07-26 22:58:58 -0500

    fix: modal

--- static/assets/js/modals.js
+++ static/assets/js/modals.js
@@ -1,168 +1,164 @@
 function closeModal(id) {
-    container = document.getElementById(`info-${id}`)
-    container.innerText = ""
-    container.style.display = "none"
-    
+  container = document.getElementById(`info-${id}`)
+  container.innerText = ""
+  container.style.display = "none"
 
-    buttonShow = document.getElementById(`buttonShow-${id}`)
-    buttonShow.style.display = 'block'
 
-    buttonLess = document.getElementById(`buttonLess-${id}`)
-    buttonLess.style.display = 'none'
+  buttonShow = document.getElementById(`buttonShow-${id}`)
+  buttonShow.style.display = 'block'
+
+  buttonLess = document.getElementById(`buttonLess-${id}`)
+  buttonLess.style.display = 'none'
 }
 
 async function openModal(id) {
-    try {
-        
-        const response = await fetch('/data_response/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCookie('csrftoken')
-            },
-            body: JSON.stringify({
-                teacher_id: id
-            })
+  try {
 
-        });
-        
-        if (!response.ok) {
-            throw new Error(`Error al recibir los datos: ${response.status}`);
-        }
+    const response = await fetch('/data_response/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': getCookie('csrftoken')
+      },
+      body: JSON.stringify({
+        teacher_id: id
+      })
 
-        const data = await response.json();
+    });
 
-        buttonShow = document.getElementById(`buttonShow-${id}`)
-        buttonShow.style.display = 'none'
+    if (!response.ok) {
+      throw new Error(`Error al recibir los datos: ${response.status}`);
+    }
 
-        container = document.getElementById(`info-${id}`)
-        container.innerText = ""
+    const data = await response.json();
 
-        var dataItem = document.createElement("div");
-            dataItem.style.display = "inline-block";
-            dataItem.style.textAlign = "center";
-            dataItem.style.margin = "20px";
+    buttonShow = document.getElementById(`buttonShow-${id}`)
+    buttonShow.style.display = 'none'
 
-        
-        if (data.projects != null) {
+    container = document.getElementById(`info-${id}`)
+    container.innerText = ""
 
-                const h5 = document.createElement('h5')
-                h5.textContent = "Proyectos"
-                h5.style.fontWeight = "bold"
-                dataItem.appendChild(h5)
-    
-    
-                data.projects.forEach(element => {
-                    var dataProject = document.createElement("p");
-                    dataProject.innerText = element.project_name;
-                    dataProject.style.textAlign = "center";
-                    dataItem.appendChild(dataProject)
-                });
-    
-            }
+    var dataItem = document.createElement("div");
+    dataItem.style.display = "inline-block";
+    dataItem.style.textAlign = "center";
+    dataItem.style.margin = "20px";
 
-        if (data.articles != null) {
 
-            const h5 = document.createElement('h5')
-            h5.textContent = "Artículos"
-            h5.style.fontWeight = "bold"
-            dataItem.appendChild(h5)
+    if (data.projects != null) {
 
-            data.articles.forEach(element => {
-                var dataArticle = document.createElement("p");
-                dataArticle.innerText = element.article_name;
-                dataArticle.style.textAlign = "center";
-                dataItem.appendChild(dataArticle)
-            });
-        }
-    
+      const h5 = document.createElement('h5')
+      h5.textContent = "Proyectos"
+      h5.style.fontWeight = "bold"
+      dataItem.appendChild(h5)
 
-        if (data.research != null) { 
 
-            const h5 = document.createElement('h5')
-            h5.textContent = "Investigaciones"
-            h5.style.fontWeight = "bold"
-            dataItem.appendChild(h5)
+      data.projects.forEach(element => {
+        var dataProject = document.createElement("p");
+        dataProject.innerText = element.project_name;
+        dataProject.style.textAlign = "center";
+        dataItem.appendChild(dataProject)
+      });
 
-            data.research.forEach(element => {
-                var dataResearch = document.createElement("p");
-                dataResearch.innerText = element.research_name;
-                dataResearch.style.textAlign = "center";
-                dataItem.appendChild(dataResearch)
-            });
+    }
 
-      
+    if (data.articles != null) {
+      const h5 = document.createElement('h5')
+      h5.textContent = "Artículos"
+      h5.style.fontWeight = "bold"
+      dataItem.appendChild(h5)
 
-        if (data.titles != null ) {
+      data.articles.forEach(element => {
+        var dataArticle = document.createElement("p");
+        dataArticle.innerText = element.article_name;
+        dataArticle.style.textAlign = "center";
+        dataItem.appendChild(dataArticle)
+      });
+    }
 
-            const h5 = document.createElement('h5');
-            h5.textContent = "Títulos Académicos";
-            h5.style.fontWeight = "bold";
-            dataItem.appendChild(h5);
-    
-            data.titles.forEach(element => {
-                const dataTitle = document.createElement("p");
-                dataTitle.innerText = element.title;  
-                dataTitle.style.textAlign = "center";
-                dataItem.appendChild(dataTitle);
-            });
-    
-        
-        }}
-        
 
-        if (!dataItem.hasChildNodes()) {
 
-            buttonLess = document.getElementById(`buttonLess-${id}`)
-            buttonLess.style.display = 'block'
-            buttonLess.onclick = null
-            var button = buttonLess.querySelector('button');
-            button.textContent = "No hay datos disponibles";
-            return
-        }
+    if (data.research != null) {
 
-        buttonLess = document.getElementById(`buttonLess-${id}`)
-        buttonLess.style.display = 'block'
+      const h5 = document.createElement('h5')
+      h5.textContent = "Investigaciones"
+      h5.style.fontWeight = "bold"
+      dataItem.appendChild(h5)
 
-        infoDivs = document.querySelectorAll('div.teacher-info')
+      data.research.forEach(element => {
+        var dataResearch = document.createElement("p");
+        dataResearch.innerText = element.research_name;
+        dataResearch.style.textAlign = "center";
+        dataItem.appendChild(dataResearch)
+      });
+    }
 
-        infoDivs.forEach(element => {
-            element.style.display = "none"
-        })
+    if (data.titles != null) {
+      const h5 = document.createElement('h5');
+      h5.textContent = "Títulos Académicos";
+      h5.style.fontWeight = "bold";
+      dataItem.appendChild(h5);
 
-        container.appendChild(dataItem);
-        container.style.display = "block"
+      data.titles.forEach(element => {
+        const dataTitle = document.createElement("p");
+        dataTitle.innerText = element.title;
+        dataTitle.style.textAlign = "center";
+        dataItem.appendChild(dataTitle);
+      });
+    }
 
-        showDivs = document.querySelectorAll('.show')
-        showDivs.forEach(element => {
 
-            if (element.id !== buttonShow.id){
-            element.style.display = "block"
-            }
-            
-        })
+    if (!dataItem.hasChildNodes()) {
 
-        lessDivs = document.querySelectorAll('.less')
-        lessDivs.forEach(element => {
-                element.style.display = "none"
-        })
+      buttonLess = document.getElementById(`buttonLess-${id}`)
+      buttonLess.style.display = 'block'
+      buttonLess.onclick = null
+      var button = buttonLess.querySelector('button');
+      button.textContent = "No hay datos disponibles";
+      return
+    }
 
-    } catch (error) {
-      console.error('ERROR:', error)
+    buttonLess = document.getElementById(`buttonLess-${id}`)
+    buttonLess.style.display = 'block'
+
+    infoDivs = document.querySelectorAll('div.teacher-info')
+
+    infoDivs.forEach(element => {
+      element.style.display = "none"
+    })
+
+    container.appendChild(dataItem);
+    container.style.display = "block"
+
+    showDivs = document.querySelectorAll('.show')
+    showDivs.forEach(element => {
+
+      if (element.id !== buttonShow.id) {
+        element.style.display = "block"
+      }
+
+    })
+
+    lessDivs = document.querySelectorAll('.less')
+    lessDivs.forEach(element => {
+      element.style.display = "none"
+    })
+
+  } catch (error) {
+    console.error('ERROR:', error)
+  }
+}
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
   }
-
-  function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+  return cookieValue;
 }
